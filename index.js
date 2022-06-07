71e7bc32b44daacd8f66c96aa5ce0647a19adb29
Brian Kimutai <brian.kimutai@student.moringaschool.com>
2022-06-07 10:09:28 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage =currentUser+ 'Welcome to Flatbook, !';
+const excitedWelcomeMessage =`${currentUser} WELCOME TO FLATBOOK, !`;
+const shortGreeting ='Welcome, ' +currentUser.slice(0,7)+ 'WELCOME TO FLATBOOK, !';
+
